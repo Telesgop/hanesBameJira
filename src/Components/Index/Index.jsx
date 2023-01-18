d93f78bdc6d9193f69031b5976e153aba119fd74
tom mann <92858999+manntom37@users.noreply.github.com>
2023-01-18 12:06:10 +0000

    HB-7 index page skeleton

--- src/Components/Index/Index.jsx
+++ src/Components/Index/Index.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { React, useContext, useState } from "react";
+import { LanguageContext } from "../../Helper/Context";
+import StoryBox from "../StoryBox/StoryBox";
+import indexapi from "./indexApi.json";
+import Copyright from "../Copyright/Copyright";
+import { Link } from "react-router-dom";
+import Pagination from "../Pagination/Pagination";
+import "./Index.css";
 
 const Index = () => {
-  return <div>Index</div>;
+  const { lang, setLang } = useContext(LanguageContext);
+  const [query, setQuery] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(12);
+
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = indexapi.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <>
+      <div className="ageRange">{lang !== true ? "Index" : "Index"}</div>
+      {/* <div className="searchbar">
+        <input
+          onChange={(event) => setQuery(event.target.value)}
+          className="searchInput"
+          placeholder={lang !== true ? "Search" : "Chwilio"}
+        ></input>
+      </div> */}
+      <div className="storiesContainer">
+        {query == "" && (
+          <table>
+            {currentPosts.map((item, id) => (
+              <Link to={item.url}>
+                <tr>
+                  <td>{item.name}</td>
+                  <td>{item.slug}</td>
+                </tr>
+              </Link>
+            ))}
+          </table>
+        )}
+      </div>
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={indexapi.length}
+        paginate={paginate}
+      ></Pagination>
+      <Copyright />
+    </>
+  );
 };
 
 export default Index;
